b3093204ab210397e8431a4a8b5f31d6f2a0b37b
Timofey Larkin <lllamnyp@gmail.com>
2021-06-08 17:09:46 +0300

    Typos

--- 4-manual-overlay.sh
+++ 4-manual-overlay.sh
@@ -3,5 +3,5 @@ set -x
 ssh vagrant.vm number=1 bash -s <network_helper.sh
 ssh opi3-router number=0 bash -s <network_helper.sh
 ssh opi3-r1-1 number=3 bash -s <network_helper.sh
-number=2 bash -s <network_helper.sh
+sudo number=2 bash -s <network_helper.sh
 
